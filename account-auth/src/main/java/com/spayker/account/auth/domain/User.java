4fa428ac4b94aa5947b68f8d147765e8402c38ec
spayker <spykerstar@gmail.com>
2020-05-12 03:34:23 +0200

    NO-TICKET removed extra import

--- account-auth/src/main/java/com/spayker/account/auth/domain/User.java
+++ account-auth/src/main/java/com/spayker/account/auth/domain/User.java
@@ -13,7 +13,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
